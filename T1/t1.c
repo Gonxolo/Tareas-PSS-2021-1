793d79fd9a45820cb848e6f92295ae5714284c9f
Gonzalo Alarcon <gonzaloalarcon@ug.uchile.cl>
2021-04-10 18:24:53 -0400

    v2: Mismo codigo, pero mas compacto

--- T1/t1.c
+++ T1/t1.c
@@ -6,48 +6,21 @@
 
 unsigned long long sumaVecs(unsigned long long vx, unsigned long long vy) {
     
-    unsigned long long mask = 0x00ff00ff00ff00ff;
-    unsigned long long total = 0, aux1 = 0, aux2 = 0;
+    unsigned long long mask = 0x00ff00ff00ff00ff, total = 0;
 
-    aux1 = vx & mask;
-    aux2 = vy & mask;
-    
-    // printf("Aux1: %llx\n", aux1);
-    // printf("Aux2: %llx\n", aux2);
-    // printf("Mask: %llx\n", mask);
+    unsigned long long aux1 = vx & mask;
+    unsigned long long aux2 = vy & mask;
 
     total |= (aux1+aux2)&mask;
-    // printf("Total: %llx\n", total);
+
     mask<<=8;
 
-    aux1 = 0;
-    aux2 = 0;
-
     aux1 = vx & mask;
     aux2 = vy & mask;
-    // printf("Aux1: %llx\n", aux1);
-    // printf("Aux2: %llx\n", aux2);
-    // printf("Mask: %llx\n", mask);
 
     total |= (aux1+aux2)&mask;
-    // printf("Total: %llx\n", total);
 
     return total;
 
 }
-
-// int main(int argc, char *argv[]){
-
-//     unsigned long long vx = 0x6500000000000000;
-//     unsigned long long vy = 0x6400000000000000;
-
-//     unsigned long long vz = sumaVecs(vx, vy);
-
-//     printf("vz: %#llx\n", vz);
-    
-
-// }
-
-// 0xff016701a1000003;
-// 0x010903ff1f0000ff;
 
